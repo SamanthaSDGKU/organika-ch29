e0b8510b5743795b3c22503630c9841084cb71d0
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2022-06-21 19:54:23 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Welcome to my super Organika store!!</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css";
+function Footer(){
+    return(
+        <div className="footer">
+            <p>All the copy rights reserved &copy; 2022 Samantha Jimenez</p>
+        </div>
+    );
+}
+
+export default Footer;
